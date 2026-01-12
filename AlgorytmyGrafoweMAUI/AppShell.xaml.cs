360e15f7347eea449cfe48a3161e1aa79a433299
Mateusz Zawadka <mateusz.zawadka@atmat.pl>
2026-01-12 10:37:34 +0100

    create project structure

---
+++ AlgorytmyGrafoweMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorytmyGrafoweMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AlgorytmyGrafoweMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AlgorytmyGrafoweMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
